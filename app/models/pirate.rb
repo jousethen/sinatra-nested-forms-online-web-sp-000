cd7726585841e094dfea7ac0f3197ce31a3e98fb
Jouse then <jthen93@gmail.com>
2021-04-21 16:18:27 +0000

    Automatically backed up by Learn

--- app/models/pirate.rb
+++ app/models/pirate.rb
@@ -3,9 +3,11 @@ class Pirate
     attr_accessor :name, :weight, :height
     @@all = []
     
-    def initialize(attributes)
-        attributes.each {|key, value| self.send(("#{key}="), value)}
-        @@all << self
+    def initialize(args)
+      @name = args[:name]
+      @weight = args[:weight]
+      @height = args[:height]
+      @@all << self
     end
 
     def self.all
