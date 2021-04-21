b578cc21a00d084fd36005045f07615391314356
Jouse then <jthen93@gmail.com>
2021-04-21 16:11:41 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -2,8 +2,7 @@ require './environment'
 
 module FormsLab
   class App < Sinatra::Base
-    
-    get "/" do
+    get '/' do
       erb :root
     end
 
@@ -14,12 +13,13 @@ module FormsLab
     post '/pirates' do
       @pirate = Pirate.new(params[:pirate])
 
-      params[:pirate][:ships].each do |ship|
-        Ship.new(ship)
+      params[:pirate][:ships].each do |details|
+        Ship.new(details)
       end
       @ships = Ship.all
 
       erb :'pirates/show'
     end
+    
   end
 end
