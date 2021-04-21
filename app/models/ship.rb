1acd3b35bc6f1f9c1d8a0a4d160e0a673401b1b0
Jouse then <jthen93@gmail.com>
2021-04-21 15:57:32 +0000

    Automatically backed up by Learn

--- app/models/ship.rb
+++ app/models/ship.rb
@@ -1,5 +1,4 @@
 class Ship
-<<<<<<< HEAD
     attr_accessor :name, :type, :booty
     @@all = []
     def initialize(attributes)
@@ -13,21 +12,5 @@ class Ship
 
     def self.clear
         @@all.clear
-=======
-  attr_accessor :name, :type, :booty
-  @@all = []
-
-    def initialize(attributes)
-        attributes.each {|key, value| self.send(("#{key}="), value)}
-        @@all < self
-    end
-    
-    def self.all
-      @@all
-    end
-    
-    def self.clear
-      @@all.clear
->>>>>>> b3b2bf25a0168e7183224668a3c8ffa68410dcf9
     end
 end
