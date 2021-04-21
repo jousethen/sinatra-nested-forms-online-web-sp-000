e0163f66eabbb01467d477ad9a0a87bc4bd2edc9
Jouse then <jthen93@gmail.com>
2021-04-21 16:05:29 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -11,7 +11,7 @@ module FormsLab
       erb :'pirates/new'
     end
 
-    post '/piratess' do
+    post '/pirates' do
       @pirate = Pirate.new(params[:pirate])
 
       params[:pirate][:ships].each do |ship|
